76155a39dd8c38fbda6420db65d58f5c01b56f88
kancho <kancho.kanev@novarto.com>
2018-04-15 13:00:43 +0300

    update initDB script - adding budgets table definition

--- sql/initDB.sql
+++ sql/initDB.sql
@@ -1,5 +1,6 @@
 USE my_budget;
 
+DROP TABLE IF EXISTS budgets;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -9,3 +10,24 @@ CREATE TABLE users (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table budgets
+(
+	id int auto_increment
+		primary key,
+	name varchar(50) not null,
+	plannedAmount decimal null,
+	spentAmount decimal null,
+	validForMonth varchar(15) null,
+	userId int not null,
+	constraint name_UNIQUE
+		unique (name),
+	constraint user
+		foreign key (userId) references my_budget.users (id)
+)
+;
+
+create index id_idx
+	on budgets (userId)
+;
+
