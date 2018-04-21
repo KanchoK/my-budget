8d4da231e963cf40d9591f9925bc4281e23b8c99
hristy93 <hristy93@gmail.com>
2018-04-21 19:49:53 +0300

    register new classes in Binder

--- src/main/java/uni/fmi/rest/binder/Binder.java
+++ src/main/java/uni/fmi/rest/binder/Binder.java
@@ -16,6 +16,14 @@ import uni.fmi.service.impl.UserServiceImpl;
 import org.glassfish.jersey.internal.inject.AbstractBinder;
 
 import javax.inject.Singleton;
+import uni.fmi.persistence.dao.CategoryDao;
+import uni.fmi.persistence.dao.PaymentDao;
+import uni.fmi.persistence.dao.impl.CategoryDaoImpl;
+import uni.fmi.persistence.dao.impl.PaymentDaoImpl;
+import uni.fmi.service.CategoryService;
+import uni.fmi.service.PaymentService;
+import uni.fmi.service.impl.CategoryServiceImpl;
+import uni.fmi.service.impl.PaymentServiceImpl;
 
 public class Binder extends AbstractBinder {
 
@@ -29,5 +37,9 @@ public class Binder extends AbstractBinder {
         bind(UserDaoImpl.class).to(UserDao.class);
         bind(BudgetDaoImpl.class).to(BudgetDao.class);
         bind(DatabaseManager.class).to(DatabaseManager.class).in(Singleton.class);
+        bind(CategoryDaoImpl.class).to(CategoryDao.class);
+        bind(PaymentDaoImpl.class).to(PaymentDao.class);
+        bind(CategoryServiceImpl.class).to(CategoryService.class);
+        bind(PaymentServiceImpl.class).to(PaymentService.class);
     }
 }
