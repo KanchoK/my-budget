f716471cda5a0fced980c07553ef865aa5c74d0c
kancho <kancho.kanev@novarto.com>
2018-04-24 17:40:12 +0300

    add CORS filter

---
+++ src/main/java/uni/fmi/filter/CorsFilter.java
@@ -0,0 +1,24 @@
+package uni.fmi.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
+
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        final int ACCESS_CONTROL_MAX_AGE_IN_SECONDS = 12 * 60 * 60;
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Headers", "origin, content-type, accept, " +
+                "authorization, username, password, userId, id, validForMonth, " +
+                "");
+        headers.add("Access-Control-Allow-Credentials", "true");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        headers.add("Access-Control-Max-Age", ACCESS_CONTROL_MAX_AGE_IN_SECONDS);
+    }
+}
